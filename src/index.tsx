732c10b7f09459af4c58a4ba907c2320035abf45
Andres <andy@mavenandy.com>
2024-03-02 01:03:04 -0600

    removed comment

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
-const root = createRoot(container!); // createRoot(container!) if you use TypeScripttab="home"
+const root = createRoot(container!);
 root.render(<App />);
